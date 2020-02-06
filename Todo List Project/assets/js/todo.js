2faf5289f92999b04f9ed237ff76bb75a4bb9218
Enoch Nordlander <50180679+enochnord@users.noreply.github.com>
2020-02-06 13:43:09 -0500

    Update todo.js

--- Todo List Project/assets/js/todo.js
+++ Todo List Project/assets/js/todo.js
@@ -1,15 +1,23 @@
 // check off specific todos by clicking
-$("li").click(function() {
-    if($(this).css("color") ==="rgb(255, 0, 0)") {
-        $(this).css({
-            color: "black",
-            textDecoration: "none" 
-        });
-    }
-    else {
-        $(this).css({
-            color: "red",
-            textDecoration: "line-through" 
-        });
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("completed");
+});
+
+// click on X to delete todo
+$("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+// press enter key on text input
+$("input[type='text']").keypress(function(event) {
+    if(event.which === 13) {
+        // get new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        // create a new li and add to ul
+        $("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + todoText + "</li>")
     }
 });
